b1c3f5a07bfc1e2bfee7f17ef5a9ce4bcec8fecc
Vini <viniczb@gmail.com>
2024-05-31 20:47:58 +0200

    Add support to typescript

--- server.ts
+++ server.ts
@@ -19,10 +19,12 @@ const MIME_TYPES = {
 const STATIC_PATH = path.join(process.cwd(), "./static");
 
 const toBool = [() => true, () => false];
-
+ // @ts-ignore
 const prepareFile = async (url) => {
   const paths = [STATIC_PATH, url];
-  if (url.endsWith("/")) paths.push("index.html");
+  if (url.endsWith("/")) {
+    paths.push("index.html")
+  };
   const filePath = path.join(...paths);
   const pathTraversal = !filePath.startsWith(STATIC_PATH);
   const exists = await fs.promises.access(filePath).then(...toBool);
@@ -33,15 +35,37 @@ const prepareFile = async (url) => {
   return { found, ext, stream };
 };
 
-http
-  .createServer(async (req, res) => {
+const router = (url: string) => {
+  switch (url) {
+    case '/':
+      return '<html><body><h1>Welcome to the Home Page</h1></body></html>';
+    case '/about':
+      return '<html><body><h1>About Us</h1></body></html>';
+    default:
+      return null;
+  }
+}
+
+
+http.createServer(async (req, res) => {
+
+  if(!req.url) return;
+
+  const routeResponse = router(req.url);
+
+  if (routeResponse !== null) {
+    res.writeHead(200, { "Content-Type": MIME_TYPES.html });
+    res.end(routeResponse);
+  } else {
     const file = await prepareFile(req.url);
     const statusCode = file.found ? 200 : 404;
+    // @ts-ignore
     const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
     res.writeHead(statusCode, { "Content-Type": mimeType });
     file.stream.pipe(res);
-    console.log(`${req.method} ${req.url} ${statusCode}`);
-  })
-  .listen(PORT);
+  }
+
+  console.log(`${req.method} ${req.url} ${routeResponse !== null ? 200 : 404}`);
+}).listen(PORT);
 
 console.log(`Server running at http://127.0.0.1:${PORT}/`);
