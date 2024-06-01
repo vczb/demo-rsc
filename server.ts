b15f52849f498530cb57e9e5cf024f2135ee07e9
Vini <viniczb@gmail.com>
2024-06-01 15:16:48 +0200

    Imprement renderToReadableStream

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import * as http from "node:http";
 import { createElement } from "react";
-import { renderToString } from "react-dom/server";
+// @ts-ignore
+import { renderToReadableStream } from 'react-dom/server.browser';
 
 const PORT = 8000;
 
@@ -16,36 +17,50 @@ const MIME_TYPES = {
   svg: "image/svg+xml",
 };
 
-const router = async (url: string) => {
+const clientComponentMap = {};
 
- const filePath = `./build/pages/${url}/page.js`.replace('//', '/');
+const router = async (url: string) => {
+  const filePath = `./build/pages/${url}/page.js`.replace('//', '/');
 
   try {
-    // @ts-ignore
     const Page = await import(filePath)
     const element = createElement(Page.default);
-    const html = renderToString(element);
-    return html
-  } catch {
-    return null
+    const stream = await renderToReadableStream(element, clientComponentMap);
+    return new Response(stream, {
+      headers: { 'content-type': 'text/html' },
+    });
+  } catch (e) {
+    console.error(e);
+    return null;
   }
-}
-
+};
 
 http.createServer(async (req, res) => {
-
-  if(!req.url) return;
+  if (!req.url) return;
 
   const routeResponse = await router(req.url);
 
   if (routeResponse !== null) {
     res.writeHead(200, { "Content-Type": MIME_TYPES.html });
-    res.end(routeResponse);
+
+    const readableStream = routeResponse.body as ReadableStream;
+    const reader = readableStream.getReader();
+
+    const streamToResponse = async () => {
+      const decoder = new TextDecoder();
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        res.write(decoder.decode(value));
+      }
+      res.end();
+    };
+
+    streamToResponse();
   } else {
     res.writeHead(404, { "Content-Type": MIME_TYPES.html });
-    res.end('Not found!')
+    res.end('Not found!');
   }
-  
 }).listen(PORT);
 
 console.log(`Server running at http://127.0.0.1:${PORT}/`);
